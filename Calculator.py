78661d7bd2225323516f880a09fd4295e9351978
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2020-03-25 23:50:55 +0200

    pycalculator

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+#Calculating the area of a circle
+
+
+# storing 3.14 in a variable called pi
+pi = 3.14
+# Accepting input (radius) from the user.
+radius = float(input("Type in the raduis of the circle :   "))
+# Using the formula to calculate the Area
+Area = (pi * radius ** 2)
+# Printing out the calculation to the console.
+print(" Area of the circle = {}".format(Area))
+
+
